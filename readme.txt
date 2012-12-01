54964592871506f1bb682b1d8d7d87446c88f68a
Claudio Sanches <contato@claudiosmweb.com>
2012-12-01 13:43:53 -0200

    initial release

---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== WooCommerce ARS Currency ===
+Contributors: claudiosanches
+Tags: ecommerce, e-commerce, commerce, wordpress ecommerce, checkout, payment, payment gateway, mercadopago
+Requires at least: 3.0
+Tested up to: 3.4.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds ARS (Argentine peso) currency in WooCommerce
+
+== Description ==
+
+### Adds ARS (Argentine peso) currency in WooCommerce
+
+Please notice that [WooCommerce](http://wordpress.org/extend/plugins/woocommerce/) must be installed and active.
+
+Works with [WooCommerce MercadoPago](http://wordpress.org/extend/plugins/woocommerce-mercadopago/).
+
+== Installation ==
+
+= Plugin Install: =
+
+* Upload plugin files to your plugins folder, or install using WordPress' built-in Add New Plugin installer
+* Activate the plugin
+* Navigate to WooCommerce -> Settings -> General and choose **Argentine peso** in **Currency** option.
+
+== License ==
+
+This file is part of WooCommerce ARS Currency.
+WooCommerce ARS Currency is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+WooCommerce ARS Currency is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Author Bio Box. If not, see <http://www.gnu.org/licenses/>.
+
+== Frequently Asked Questions ==
+
+= What is the plugin license? =
+
+* This plugin is released under a GPL license.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+* Enjoy it.
+
+== Screenshots ==
+
+1. screenshot-1.png
---
+++ wc-ars-currency.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Plugin Name: WooCommerce ARS Currency
+ * Plugin URI: http://claudiosmweb.com/
+ * Description: Adds ARS (Argentine peso) currency in WooCommerce
+ * Author: claudiosanches
+ * Author URI: http://www.claudiosmweb.com/
+ * Version: 1.0
+ * License: GPLv2 or later
+ */
+
+if ( !class_exists( 'WC_ARS_Currency' ) ) {
+
+    /**
+     * Add ARS Currency in WooCommerce.
+     */
+    class WC_ARS_Currency {
+
+        /**
+         * Class construct.
+         */
+        public function __construct() {
+
+            // Actions
+            add_action( 'plugins_loaded', array( &$this, 'load_textdomain' ), 0 );
+
+            // Filters.
+            add_filter( 'woocommerce_currencies', array( &$this, 'add_currency' ) );
+            add_filter( 'woocommerce_currency_symbol', array( &$this, 'currency_symbol' ), 1, 2 );
+        }
+
+        /**
+         * Load Plugin textdomain.
+         *
+         * @return void.
+         */
+        public function load_textdomain() {
+            load_plugin_textdomain( 'wcars', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+        }
+
+        /**
+         * Add ARS Currency in WooCommerce.
+         *
+         * @param  array $currencies Current currencies.
+         *
+         * @return array             Currencies with ARS.
+         */
+        public function add_currency( $currencies ) {
+            $currencies['ARS'] = __( 'Argentine peso (&#36;)', 'wcars' );
+            asort( $currencies );
+
+            return $currencies;
+        }
+
+        /**
+         * Add ARS Symbol
+         *
+         * @param  string $currency_symbol Currency symbol.
+         * @param  array  $currency        Current currencies.
+         *
+         * @return string                  ARS currency symbol.
+         */
+        public function currency_symbol( $currency_symbol, $currency ) {
+            switch( $currency ) {
+                case 'ARS':
+                    $currency_symbol = '&#36;';
+                    break;
+            }
+
+            return $currency_symbol;
+        }
+
+    } // close WC_ARS_Currency class.
+
+    $WC_ARS_Currency = new WC_ARS_Currency();
+}
